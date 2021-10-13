988512bb4982e24aa5ad3645ec31bc9349f92f57
unknown <alexis.chevet@student.yncrea.fr>
2021-10-13 19:30:29 +0200

    ACT clique pour la droite

--- TP2/Droite.js
+++ TP2/Droite.js
@@ -2,7 +2,7 @@ const line = new THREE.LineBasicMaterial({ //définition taille et couleurs des 
     linewidth: 0.05,
     color: 0x0000FF
 })
-const droite = []; // Tableau de points pour la droite
+const droite = [new THREE.Vector3(0,0,0), new THREE.Vector3(-15, 3.8, 0)]; // Tableau de points pour la droite
 
 let coordonnees=document.getElementById("coordonnees");
 
@@ -16,17 +16,17 @@ renderer.domElement.addEventListener("click", function(e) //Add event to our can
 
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();    //Get the size of canvas and his position on the screen 
-    let Xaxis = event.clientX - rect.left;        //Take the X coordonate of the click and subtract it from the left side of the canvas
-    let Yaxis = event.clientY - rect.top;
-    droite.push( new THREE.Vector3( Xaxis, Yaxis, 0 ));
+    let Xaxis = event.clientX - rect.left - (canvas.width/2);        //Take the X coordonate of the click and subtract it from the left side of the canvas
+    let Yaxis = rect.top + (canvas.height/2) - event.clientY;
+    droite.push( new THREE.Vector3( Xaxis/(128/3), Yaxis/(2335/38), 0 ));
     afficherDroite();
-    console.log("Coordinate x: " + Xaxis + "Coordinate y: " + Yaxis);
+    console.log(event.clientX + " - " + rect.left + " - " + (canvas.width/2) + " = " + Xaxis/(128/3) + " " + rect.top + " + " + (canvas.height/2) + " - " + event.clientY + " = " + Yaxis/(2335/38));
 }
 
 function afficherDroite(){
-    let StringofPoint=coordonnees;
+    /*let StringofPoint=coordonnees;
     let reg=new RegExp("[ ,;]+", "g");
-    let tableau=StringofPoint.split(reg);
+    let tableau=StringofPoint.split(reg);*/
    // for(let sizeof
     console.log(droite);
     if(droite.lenght < 2) return; // On vérifie qu'on a assez de points pour faire un droite
