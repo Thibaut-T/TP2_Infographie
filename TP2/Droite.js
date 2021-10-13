eb853120907ea6cd0ad246c0f0475dffe5dc32e7
Thibaut-T <thibauttournemaine@gmail.com>
2021-10-13 20:41:59 +0200

    m

--- TP2/Droite.js
+++ TP2/Droite.js
@@ -23,16 +23,14 @@ function afficherDroite(){
     console.log(coordonnees);
     let StringofPoint=coordonnees;
     let tableau=StringofPoint.split(' ');
-    console.log(tableau[0]);
+    console.log(tableau);
     for(let i=0;i<tableau.length;i++){
         droite.push(new THREE.Vector3(tableau[i],tableau[i+1], 0 ) );
-        console.log(droite[droite.length-1]); // PROOOOOOOBLEME
         i++;
     }
-    
-    console.log(THREE.Vector3);
+    console.log(droite);
     afficherCourbe(droite);
-   /* if(droite.lenght < 2) return; // On vérifie qu'on a assez de points pour faire un droite
+  /*  if(droite.lenght < 2) return; // On vérifie qu'on a assez de points pour faire un droite
     const geometry = new THREE.BufferGeometry().setFromPoints(droite);  // On ajoute au buffer
     const figure = new THREE.Line( geometry, line );
     scene.add( figure ); // on ajoute à la scène tous les droites
