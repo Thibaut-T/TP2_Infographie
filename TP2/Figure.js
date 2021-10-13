960750361cf64dd78d27550852165eb9f9c5e7dc
Thibaut-T <thibauttournemaine@gmail.com>
2021-10-13 14:52:18 +0200

    Add Point function

--- TP2/Figure.js
+++ TP2/Figure.js
@@ -18,20 +18,16 @@ const line = new THREE.LineBasicMaterial({ //définition taille et couleurs des 
 const points = []; // tableau de points
 const droite = []; // Tableau de points pour la droite
 
+let coordonnees=document.getElementById("coordonnees");
+
+let button_coordonnees=document.getElementById("coordonnees")
+button_coordonnees.addEventListener("click",afficherDroite);
+
 renderer.domElement.addEventListener("click", function(e) //Add event to our canvas 
 {
     getMousePosition(renderer.domElement, e);      //If their is ²a click, call the function 
 });
 
-
-function getMousePosition(canvas, event) {
-    let rect = canvas.getBoundingClientRect();    //Get the size of canvas and his position on the screen 
-    let Xaxis = event.clientX - rect.left;        //Take the X coordinate of the click and subtract it from the left side of the canvas
-    let Yaxis = event.clientY - rect.top;
-    droite.push( new THREE.Vector3( Xaxis, Yaxis, 0 ) );
-    afficherDroite();
-    console.log("Coordinate x: " + Xaxis + "Coordinate y: " + Yaxis);
-}
 
 let button_reset=document.getElementById("reset");
 button_reset.addEventListener("click",reset);
@@ -45,6 +41,15 @@ let coeur=document.getElementById("coeur");
 
 cercle.addEventListener("click",drawCircle);
 coeur.addEventListener("click",drawHeart);
+
+function getMousePosition(canvas, event) {
+    let rect = canvas.getBoundingClientRect();    //Get the size of canvas and his position on the screen 
+    let Xaxis = event.clientX - rect.left;        //Take the X coordonate of the click and subtract it from the left side of the canvas
+    let Yaxis = event.clientY - rect.top;
+    droite.push( new THREE.Vector3( Xaxis, Yaxis, 0 ) );
+    afficherDroite();
+    console.log("Coordinate x: " + Xaxis + "Coordinate y: " + Yaxis);
+}
 
 function reset(){
     document.location.reload(true);
@@ -142,6 +147,11 @@ function evaluate(t, str){
 }
 
 function afficherDroite(){
+    let StringofPoint=coordonnees;
+    let reg=new RegExp("[ ,;]+", "g");
+    let tableau=StringofPoint.split(reg);
+    let Xpoint=tableau[0];
+    let Ypoint=tableau[1];
     console.log(droite);
     if(droite.lenght < 2) return; // On vérifie qu'on a assez de points pour faire un droite
     const geometry = new THREE.BufferGeometry().setFromPoints(droite);  // On ajoute au buffer
